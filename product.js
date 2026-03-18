acc56a9b49ab3a42b2ba41362a47407fc37cc63c
Billy008-001 <bgathecere2@gmail.com>
2026-03-18 13:26:45 +0300

    Add product page with CSS styling and JavaScript object for fruit juices
    - Created product.html for the product display layout.
    - Added product.css for styling the product page.
    - Introduced product.js with a JavaScript object structure for fruit juice details.

---
+++ product.js
@@ -0,0 +1,9 @@
+const fruitjuice = {
+    juicename: "grapefruit",
+    tag: "Sweet and delicious a good way to start your day",
+    juiceimage:"Images/Grape fruit juice.jpg"
+},
+{
+    juicename:,
+    tag:,
+}
